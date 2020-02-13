8e881310865825a67f0018e8560f4e9f23e18cd5
Mr. Matt <matt@composition9.com>
2020-02-12 20:02:15 -0700

    create symlinks

--- main.go
+++ main.go
@@ -8,19 +8,23 @@ import (
 	"strings"
 )
 
-const SUFFIX = ".linkre"
+// Suffix is the file suffix that indicates to the program to create a symlink of that file
+const Suffix = ".linkre"
 
 func main() {
 	flag.Parse()
 	root := flag.Arg(0)
 
 	visit := func(path string, f os.FileInfo, err error) error {
-		if filepath.Ext(path) == SUFFIX {
-			fmt.Printf("Making a symlink of %s\n", path)
+		if filepath.Ext(path) == Suffix {
 			toPath := strings.TrimPrefix(
-				strings.TrimSuffix(path, SUFFIX), root,
+				strings.TrimSuffix(path, Suffix), root,
 			)
+			fmt.Printf("Making a symlink of %s\n", path)
 			fmt.Printf("to: %s\n", toPath)
+			if err := os.Symlink(path, toPath); err != nil {
+				return err
+			}
 		}
 		return nil
 	}
